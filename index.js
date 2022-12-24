e000492e94d1c122025725ad6cdce4e2b0df9f31
Eugene-1ee <eugene5443@naver.com>
2022-12-24 21:57:01 +0900

    Update index.js
    Ignore on Error

--- index.js
+++ index.js
@@ -46,4 +46,17 @@ for ( const file of eventFiles )
     }
 }
 
+process.on( "unhandledRejection", async ( err ) => {
+    //console.error( "Unhandled Promise Rejection:\n", err );
+});
+process.on( "uncaughtException", async ( err ) => {
+    //console.error( "Uncaught Promise Exception:\n", err );
+});
+process.on( "uncaughtExceptionMonitor", async ( err ) => {
+  //console.error( "Uncaught Promise Exception (Monitor):\n", err );
+});
+process.on( "multipleResolves", async ( type, promise, reason ) => {
+  //console.error( "Multiple Resolves:\n", type, promise, reason );
+});
+
 client.login( token );
