38a8c358a7d587e299e89cb9cd837829608047f2
Eugene-1ee <eugene5443@naver.com>
2023-05-07 16:01:42 +0900

    Update monitor.js
    Error handling

--- events/debug/monitor.js
+++ events/debug/monitor.js
@@ -1,8 +1,22 @@
 module.exports =
 {
-    name : "uncaughtExceptionMonitor",
+    name : 'uncaughtExceptionMonitor',
     async execute( error )
     {
-        console.log( "Uncaught Promise Exception (Monitor):\n", error.message );
+        console.log( 'Uncaught Promise Exception (Monitor):', error.message );
+
+        if ( error.message == 'aborted' )
+        {
+            console.log( '긴 영상 재생 끊김.' );
+
+            const interaction = error.resource.interaction;
+
+            interaction.channel.send( "너무 긴 영상을 재생했나봐\n오류 발생!" );
+            return;
+        }
+        else
+        {
+            return;
+        }
     }
 }
