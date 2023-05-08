a072783870179736e158606cb2b5226b53a5b380
Eugene-1ee <eugene5443@naver.com>
2023-05-09 02:05:43 +0900

    Update val.js

--- functions/val.js
+++ functions/val.js
@@ -2,7 +2,6 @@ let connection = new Map( );
 let player = new Map( );
 let playlist = new Map( );
 let resource = new Map( );
-let volume = new Map( );
 let station = new Map( );
 
 module.exports =
@@ -11,6 +10,5 @@ module.exports =
     player: player,
     playlist: playlist,
     resource: resource,
-    volume: volume,
     station: station
 };
