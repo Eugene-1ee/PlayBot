bc16587165865e029f19565441a871acb2fbd4cf
Eugene-1ee <eugene5443@naver.com>
2023-05-09 02:05:18 +0900

    Update timeConvert.js

--- util/timeConvert.js
+++ util/timeConvert.js
@@ -10,9 +10,9 @@
         return '[Roading]';
     }
     const hour = parseInt( seconds / 3600 ) == 0 ?
-         '' : `${ parseInt( seconds / 3600 ) }:` ;
+         '' : `${ parseInt( seconds / 3600 ) }:`;
     const min = parseInt( ( seconds % 3600 ) / 60 ) == 0 ?
-        ( hour == ''? '0:': '00:' ) : ( parseInt( ( seconds % 3600 ) / 60 ) < 10 && hour !== '' ? `0${ parseInt( ( seconds % 3600 ) / 60 ) }:` : `${ parseInt( ( seconds % 3600 ) / 60 ) }:` );
+        ( hour == '' ? '0:': '00:' ) : ( parseInt( ( seconds % 3600 ) / 60 ) < 10 && hour !== '' ? `0${ parseInt( ( seconds % 3600 ) / 60 ) }:` : `${ parseInt( ( seconds % 3600 ) / 60 ) }:` );
     const sec = seconds % 60 == 0 ?
         '00' : ( seconds % 60 < 10 ? `0${ seconds % 60 }` : `${ seconds % 60 }` );
      
