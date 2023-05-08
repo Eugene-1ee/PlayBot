78bfa0c38a6f39d3481ab7adb5d3f2862a16cde7
Eugene-1ee <eugene5443@naver.com>
2023-05-09 02:05:36 +0900

    Update button.js

--- util/button.js
+++ util/button.js
@@ -140,8 +140,75 @@ function pagemore( interaction, now, max )
     return row;
 }
 
+/**
+ * 검색 버튼 생성기
+ * @param {} interaction
+ * @returns 버튼
+ */
+function searchButton( interaction )
+{
+    const row = [] 
+    row[0] = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+        .setCustomId( '1' )
+        .setLabel( '1' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '2' )
+        .setLabel( '2' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '3' )
+        .setLabel( '3' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '4' )
+        .setLabel( '4' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '5' )
+        .setLabel( '5' )
+        .setStyle( ButtonStyle.Secondary ) );
+
+    row[1] = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+        .setCustomId( '6' )
+        .setLabel( '6' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '7' )
+        .setLabel( '7' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '8' )
+        .setLabel( '8' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '9' )
+        .setLabel( '9' )
+        .setStyle( ButtonStyle.Secondary ),
+        new ButtonBuilder()
+        .setCustomId( '10' )
+        .setLabel( '10' )
+        .setStyle( ButtonStyle.Secondary ) );
+
+    row[2] =  new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+        .setCustomId( 'cancel' )
+        .setLabel( '취소' )
+        .setStyle( ButtonStyle.Danger ),
+        new ButtonBuilder()
+        .setCustomId( 'InteractionUserName' )
+        .setLabel( `Requested by ${interaction.user.username + '#' + interaction.user.discriminator}` )
+        .setStyle( ButtonStyle.Secondary )
+        .setDisabled( true ) );
+    
+    return row;
+}
+
 module.exports =
 {
     Buttonpage2: Buttonpage2,
-    pagemore: pagemore
+    pagemore: pagemore,
+    searchButton: searchButton
 };
