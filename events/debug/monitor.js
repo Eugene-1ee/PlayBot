5e7c38235a721750df000c8858ac220dedcdfcd6
Eugene-1ee <eugene5443@naver.com>
2023-05-09 02:06:34 +0900

    Update monitor.js

--- events/debug/monitor.js
+++ events/debug/monitor.js
@@ -5,18 +5,6 @@ module.exports =
     {
         console.log( 'Uncaught Promise Exception (Monitor):', error.message );
 
-        if ( error.message == 'aborted' )
-        {
-            console.log( '긴 영상 재생 끊김.' );
-
-            const interaction = error.resource.interaction;
-
-            interaction.channel.send( "너무 긴 영상을 재생했나봐\n오류 발생!" );
-            return;
-        }
-        else
-        {
-            return;
-        }
+        return;
     }
 }
