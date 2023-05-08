dd67528dff469419f11120d647341e4892260972
Eugene-1ee <eugene5443@naver.com>
2023-05-09 02:06:30 +0900

    Update rejection.js

--- events/debug/rejection.js
+++ events/debug/rejection.js
@@ -3,7 +3,7 @@ module.exports =
     name : 'unhandledRejection',
     async execute( error )
     {
-        console.log( 'Unhandled Promise Rejection:\n', error.message );
+        console.log( 'Unhandled Promise Rejection:\n', error );
         return;
     }
 }
