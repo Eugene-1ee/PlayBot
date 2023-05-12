414519a838ea2e81c7696ea99896e299b5594e61
Eugene-1ee <eugene5443@naver.com>
2023-05-13 01:03:26 +0900

    Update timeConvert.js

--- util/timeConvert.js
+++ util/timeConvert.js
@@ -3,21 +3,21 @@
  * @param { number } seconds input seconds
  * @returns x:xx:xx
  */
- function timeConvert( seconds )
- {
+function timeConvert( seconds )
+{
     if ( !seconds )
     {
         return '[Roading]';
     }
     const hour = parseInt( seconds / 3600 ) == 0 ?
-         '' : `${ parseInt( seconds / 3600 ) }:`;
+        '' : `${ parseInt( seconds / 3600 ) }:`;
     const min = parseInt( ( seconds % 3600 ) / 60 ) == 0 ?
         ( hour == '' ? '0:': '00:' ) : ( parseInt( ( seconds % 3600 ) / 60 ) < 10 && hour !== '' ? `0${ parseInt( ( seconds % 3600 ) / 60 ) }:` : `${ parseInt( ( seconds % 3600 ) / 60 ) }:` );
     const sec = seconds % 60 == 0 ?
         '00' : ( seconds % 60 < 10 ? `0${ seconds % 60 }` : `${ seconds % 60 }` );
-     
+
     const time = hour + min + sec;
     return time;
- };
- 
- module.exports = timeConvert;
+};
+
+module.exports = timeConvert;
